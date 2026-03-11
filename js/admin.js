62a9f5e0dfe804abbcc0d7cbf0533677169b59c4
EN7DESIGN <e.n7design@gmail.com>
2026-03-11 13:09:35 +0100

    update admin

--- js/admin.js
+++ js/admin.js
@@ -2,7 +2,7 @@ let allResponses = [];
 let currentSort = 'date';
 let isAsc = false; // Par défaut : récent en haut (desc)
 let currentPage = 1;
-const itemsPerPage = 2;
+const itemsPerPage = 5;
 
 
 async function loadResponses() {
